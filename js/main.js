a4f984ebf9397a862a9272c98592a0de7b10f5ab
dobelinn <bruno.dobelin13@gmail.com>
2021-11-07 20:30:20 -0300

    Bruno Changes in Lista.html

--- js/main.js
+++ js/main.js
@@ -22,3 +22,17 @@ window.addEventListener("scroll", function () {
         document.querySelector("#navbar").style.opacity = 1;
     }
 });
+const controller = document.querySelector('input[type=range]');
+const radialProgress = document.querySelector('.RadialProgress');
+
+const setProgress = (progress) => {
+  const value = `${progress}%`;
+  radialProgress.style.setProperty('--progress', value)
+  radialProgress.innerHTML = value
+  radialProgress.setAttribute('aria-valuenow', value)
+}
+
+setProgress(controller.value)
+controller.oninput = () => {
+  setProgress(controller.value)
+}
